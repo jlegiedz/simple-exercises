0f27be5316c2119a2cbbcce1c7df147010e73842
jlegiedz <joanna.legiedz@gmail.com>
2016-02-06 22:11:48 +0100

    next one

---
+++ checkElement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 8, 3, 4, 7, 6, 5 };
+
+            int a = 1;
+            
+            for (int i = 0; i < arr.Length; i++)
+            
+            {
+               if (arr[i] == a)
+                {
+                  
+                    Console.WriteLine("The element you are looking for is in the list in " + i + " place");
+                }
+         
+            }
+            Console.ReadLine();
+        }
+    }
+}
