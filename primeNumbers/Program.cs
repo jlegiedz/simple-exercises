119b6707003852d860081f489369edfa504c56a8
jlegiedz <joanna.legiedz@gmail.com>
2016-02-06 21:24:38 +0100

    Initial commit

---
+++ primeNumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] a = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
+            var b = new ArrayList(a);
+
+            int n = 30;
+
+            for (int i = 2; i <= Math.Floor(Math.Sqrt(n)); i++)
+            {
+                for (int j = 1; j< n-1; j++)
+                {
+                    if (b[j] != null)
+                    {
+                        int c = (int)b[j];
+                        
+
+                        if (c % i == 0) 
+                        {
+                            if (c > i)
+                            {
+                                b[j] = null;
+                            }
+                        }
+                    }
+                }
+            }
+            foreach (object obj in b)
+            {
+                if (obj != null)
+                {
+                    int d = (int)obj;
+                    Console.Write(d.ToString("00 "));
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ reverseInPlace/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseInPlace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 6, 3, 9, 7, 4};
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write(num[i]+" ");
+            }
+
+            Console.WriteLine();
+
+            int n = (int)Math.Floor(num.Length / 2.0);
+
+            int a;
+            for (int i =0; i < n; i++)
+            {
+                a = num[i];
+                num[i] = num[num.Length - i - 1];
+                num[num.Length - i - 1] = a;
+
+            }
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.Write(num[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
