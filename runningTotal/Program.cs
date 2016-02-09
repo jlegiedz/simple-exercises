e69c38befe06fedf1c43366c19ce4bc4c11a6a93
jlegiedz <joanna.legiedz@gmail.com>
2016-02-09 22:46:30 +0100

    running total

---
+++ runningTotal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace runningTotal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int sum = 0;
+
+            for (int i = 0; i <= num.Length; i++)
+            {
+                sum = sum + i;
+               
+            }
+            Console.Write(sum);
+            Console.ReadLine();
+        }
+     
+    }
+}
