ef4830c483927e667fbe034bac75f01111ec1df1
jlegiedz <joanna.legiedz@gmail.com>
2016-02-09 22:23:15 +0100

    show odd elements

---
+++ showOddelements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace showOddelements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] letters = { "a", "b", "c", "d", "e", "f" };
+          
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(letters[i]+" ");
+                 
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
